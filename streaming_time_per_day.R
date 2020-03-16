37a8b255b9d2a1731d0aaa140ce5c7c4b94b0822
PeerChristensen <hr.pchristensen@gmail.com>
2020-03-16 10:35:46 +0100

    x

--- streaming_time_per_day.R
+++ streaming_time_per_day.R
@@ -1,29 +1,31 @@
 
 library(tidyverse)
-library(RODBC)
+# library(RODBC)
+# 
+# query <- "select
+# 
+# *
+# --sum(CAST(MillisecondsSpent AS BIGINT)) as msec
+# 
+# FROM EDW.edw.ReadDurationFact
+# 
+# order by ReadDurationDate_Key" 
+# 
+# 
+# credentials <- read_rds("credentials.rds")
+# 
+# channel <- odbcConnect(credentials[1],credentials[2],credentials[3])
+# 
+# df <- sqlQuery(channel,query) %>%
+#   as_tibble() %>%
+#   mutate(Date = lubridate::ymd(ReadDurationDate_Key)) %>%
+#   select(Date,Customer_Key,Product_Key,msec = MillisecondsSpent) %>%
+#   mutate(msec = as.numeric(msec)) %>%
+#   filter( Date < lubridate::today()-3,
+#           Customer_Key > 0)
 
-query <- "select
 
-*
---sum(CAST(MillisecondsSpent AS BIGINT)) as msec
-
-FROM EDW.edw.ReadDurationFact
-
-order by ReadDurationDate_Key" 
-
-
-credentials <- read_rds("credentials.rds")
-
-channel <- odbcConnect(credentials[1],credentials[2],credentials[3])
-
-df <- sqlQuery(channel,query) %>%
-  as_tibble() %>%
-  mutate(Date = lubridate::ymd(ReadDurationDate_Key)) %>%
-  select(Date,Customer_Key,Product_Key,msec = MillisecondsSpent) %>%
-  mutate(msec = as.numeric(msec)) %>%
-  filter( Date < lubridate::today()-3,
-          Customer_Key > 0)
-
+df <- read_csv("clean_duration_data.csv")
 
 # avg tid per kunde per dag
 tid <- df %>%
@@ -40,6 +42,15 @@ tid %>%
   geom_line() +
   theme_minimal() +
   ylim(c(0,150))
+
+# median og avg tid per kunde per dag
+df %>%
+  group_by(Date,Customer_Key) %>%
+  summarise(t = sum(msec)/ 1000 / 60) %>%
+  group_by(Date) %>%
+  summarise(median = median(t),
+            mean = mean(t))
+  
 
 # avg antal titler per kunde per dag
 n_titler <- df %>% 
